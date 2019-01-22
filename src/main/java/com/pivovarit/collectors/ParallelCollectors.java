41240d97ea86dfd105e8ea6ddf0fa1b2f323203d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-01-22 20:18:43 +0100

    Accept operations as parameters (#20)
    * Operations as inputs
    
    * Rename ParallelCollectionMappingCollector to ParallelCollectionCollector
    
    * Rename generic parameters

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 
@@ -19,14 +20,26 @@ public final class ParallelCollectors {
     }
 
     public static <T, R extends Collection<T>> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<R>> toCollectionInParallel(Supplier<R> collection, Executor executor) {
-        return new ParallelCollectionCollector<>(executor, collection);
+        return new ParallelMappingCollector<>(Supplier::get, executor, collection);
+    }
+
+    public static <T, R, C extends Collection<R>> Collector<T, List<CompletableFuture<R>>, CompletableFuture<C>> toCollectionInParallel(Function<T, R> operation, Supplier<C> collection, Executor executor) {
+        return new ParallelMappingCollector<>(operation, executor, collection);
     }
 
     public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> toListInParallel(Executor executor) {
-        return new ParallelCollectionCollector<>(executor, ArrayList::new);
+        return new ParallelMappingCollector<>(Supplier::get, executor, ArrayList::new);
+    }
+
+    public static <T, R> Collector<T, List<CompletableFuture<R>>, CompletableFuture<List<R>>> toListInParallel(Function<T, R> operation, Executor executor) {
+        return new ParallelMappingCollector<>(operation, executor, ArrayList::new);
     }
 
     public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<Set<T>>> toSetInParallel(Executor executor) {
-        return new ParallelCollectionCollector<>(executor, HashSet::new);
+        return new ParallelMappingCollector<>(Supplier::get, executor, HashSet::new);
+    }
+
+    public static <T, R> Collector<T, List<CompletableFuture<R>>, CompletableFuture<Set<R>>> toSetInParallel(Function<T, R> operation, Executor executor) {
+        return new ParallelMappingCollector<>(operation, executor, HashSet::new);
     }
 }
---
+++ src/test/java/com/pivovarit/collectors/ParallelCollectorsMappingTest.java
@@ -0,0 +1,91 @@
+package com.pivovarit.collectors;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.After;
+import org.junit.runner.RunWith;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
+import static com.pivovarit.collectors.ParallelCollectors.supplier;
+import static com.pivovarit.collectors.ParallelCollectors.toCollectionInParallel;
+import static com.pivovarit.collectors.ParallelCollectors.toListInParallel;
+import static com.pivovarit.collectors.ParallelCollectors.toSetInParallel;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+@RunWith(JUnitQuickcheck.class)
+public class ParallelCollectorsMappingTest {
+
+    private static final int TRIALS = 10;
+    private static final int BLOCKING_MILLIS = 200;
+    private static final int TIMEOUT = (int) (BLOCKING_MILLIS * 1.5);
+
+    private ExecutorService executor;
+
+    @Property(trials = TRIALS)
+    public void shouldCollectToListWithFullParallelism(@InRange(minInt = 10, maxInt = 100) int collectionSize) {
+        // given
+        executor = Executors.newFixedThreadPool(collectionSize);
+
+        List<Integer> result = assertTimeout(Duration.ofMillis(TIMEOUT), () ->
+          Stream.generate(() -> 42)
+            .limit(collectionSize)
+            .collect(toListInParallel(i -> blockingFoo(), executor))
+            .join());
+
+        assertThat(result).hasSize(collectionSize);
+    }
+
+    @Property(trials = TRIALS)
+    public void shouldCollectToSetWithFullParallelism(@InRange(minInt = 10, maxInt = 100) int collectionSize) {
+        // given
+        executor = Executors.newFixedThreadPool(collectionSize);
+
+        Set<Integer> result = assertTimeout(Duration.ofMillis(TIMEOUT), () ->
+          Stream.generate(() -> 42)
+            .limit(collectionSize)
+            .collect(toSetInParallel(i -> blockingFoo(), executor))
+            .join());
+
+        assertThat(result).hasSize(1);
+    }
+
+    @Property(trials = TRIALS)
+    public void shouldCollectToCollectionWithFullParallelism(@InRange(minInt = 10, maxInt = 100) int collectionSize) {
+        // given
+        executor = Executors.newFixedThreadPool(collectionSize);
+
+        List<Integer> result = assertTimeout(Duration.ofMillis(TIMEOUT), () ->
+          Stream.generate(() -> 42)
+            .limit(collectionSize)
+            .collect(toCollectionInParallel(i -> blockingFoo(), ArrayList::new, executor))
+            .join());
+
+        assertThat(result).hasSize(collectionSize);
+    }
+
+    @After
+    public void after() {
+        if (executor != null) {
+            executor.shutdown();
+        }
+    }
+
+    private static int blockingFoo() {
+        try {
+            Thread.sleep(BLOCKING_MILLIS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return 42;
+    }
+}
