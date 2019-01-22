b31b67624dfe9740f7d38f4fc515a3310524ee9b
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-01-22 18:02:26 +0100

    Add toCollection() collector (#16)

--- src/test/java/com/pivovarit/collectors/ParallelCollectorsTest.java
+++ src/test/java/com/pivovarit/collectors/ParallelCollectorsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -11,12 +12,13 @@ import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 import static com.pivovarit.collectors.ParallelCollectors.supplier;
+import static com.pivovarit.collectors.ParallelCollectors.toCollectionInParallel;
 import static com.pivovarit.collectors.ParallelCollectors.toListInParallel;
 import static com.pivovarit.collectors.ParallelCollectors.toSetInParallel;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
-class ParallelCollectionCollectorTest {
+class ParallelCollectorsTest {
 
     private ExecutorService executor;
 
@@ -48,6 +50,21 @@ class ParallelCollectionCollectorTest {
             .join());
 
         assertThat(result).hasSize(1);
+    }
+
+    @Test
+    void shouldCollectToCollectionWithFullParallelism() {
+        // given
+        int collectionSize = 200;
+        executor = Executors.newFixedThreadPool(collectionSize);
+
+        List<Integer> result = assertTimeout(Duration.ofMillis(1100), () ->
+          Stream.generate(() -> supplier(() -> blockingFoo()))
+            .limit(collectionSize)
+            .collect(toCollectionInParallel(ArrayList::new, executor))
+            .join());
+
+        assertThat(result).hasSize(collectionSize);
     }
 
     @AfterEach
