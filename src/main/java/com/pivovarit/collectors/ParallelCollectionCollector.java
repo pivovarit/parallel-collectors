c18b8c744191500e072cc78081c07cd7538ca2c4
pivovarit <gpiwowarek@gmail.com>
2019-01-22 17:42:44 +0100

    Implement a generic parallel collector

--- src/main/java/com/pivovarit/collectors/ParallelCollectionCollector.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectionCollector.java
@@ -1,26 +1,31 @@
 package com.pivovarit.collectors;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
-class ParallelCollectionCollector<T>
-  implements Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> {
+class ParallelCollectionCollector<T, C extends Collection<T>>
+  implements Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<C>> {
 
     private final Executor executor;
+    private final Supplier<C> collectionSupplier;
 
-    ParallelCollectionCollector(Executor executor) {
+    ParallelCollectionCollector(Executor executor, Supplier<C> collection) {
         this.executor = executor;
+        this.collectionSupplier = collection;
     }
 
     @Override
@@ -30,7 +35,7 @@ class ParallelCollectionCollector<T>
 
     @Override
     public BiConsumer<List<CompletableFuture<T>>, Supplier<T>> accumulator() {
-        return (processing, supplier) -> processing.add(CompletableFuture.supplyAsync(supplier, executor));
+        return (processing, supplier) -> processing.add(supplyAsync(supplier, executor));
     }
 
     @Override
@@ -42,14 +47,29 @@ class ParallelCollectionCollector<T>
     }
 
     @Override
-    public Function<List<CompletableFuture<T>>, CompletableFuture<List<T>>> finisher() {
-        return futures -> futures.stream().reduce(completedFuture(new ArrayList<>()),
-          (list, object) -> list.thenCombine(object, (left, right) -> { left.add(right); return left; }),
-          (f1, f2) -> f1.thenCombine(f2, (left, right) -> { left.addAll(right); return left; }));
+    public Function<List<CompletableFuture<T>>, CompletableFuture<C>> finisher() {
+        return futures -> futures.stream()
+          .reduce(completedFuture(collectionSupplier.get()),
+            accumulatingResults(),
+            mergingPartialResults());
     }
 
     @Override
     public Set<Characteristics> characteristics() {
-        return new HashSet<>();
+        return EnumSet.of(Characteristics.UNORDERED);
+    }
+
+    private BinaryOperator<CompletableFuture<C>> mergingPartialResults() {
+        return (f1, f2) -> f1.thenCombine(f2, (left, right) -> {
+            left.addAll(right);
+            return left;
+        });
+    }
+
+    private BiFunction<CompletableFuture<C>, CompletableFuture<T>, CompletableFuture<C>> accumulatingResults() {
+        return (list, object) -> list.thenCombine(object, (left, right) -> {
+            left.add(right);
+            return left;
+        });
     }
 }
--- src/test/java/com/pivovarit/collectors/ParallelCollectionCollectorTest.java
+++ src/test/java/com/pivovarit/collectors/ParallelCollectionCollectorTest.java
@@ -4,30 +4,57 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 import static com.pivovarit.collectors.ParallelCollectors.supplier;
 import static com.pivovarit.collectors.ParallelCollectors.toListInParallel;
+import static com.pivovarit.collectors.ParallelCollectors.toSetInParallel;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class ParallelCollectionCollectorTest {
 
-    private ExecutorService executor = Executors.newFixedThreadPool(200);
+    private ExecutorService executor;
 
     @Test
-    void shouldExecuteWithFullParallelism() {
-        assertTimeout(Duration.ofMillis(1050), () ->
+    void shouldCollectToListWithFullParallelism() {
+        // given
+        int collectionSize = 200;
+        executor = Executors.newFixedThreadPool(collectionSize);
+
+        List<Integer> result = assertTimeout(Duration.ofMillis(1100), () ->
           Stream.generate(() -> supplier(() -> blockingFoo()))
-            .limit(200)
+            .limit(collectionSize)
             .collect(toListInParallel(executor))
             .join());
+
+        assertThat(result).hasSize(collectionSize);
+    }
+
+    @Test
+    void shouldCollectToSetWithFullParallelism() {
+        // given
+        int collectionSize = 200;
+        executor = Executors.newFixedThreadPool(collectionSize);
+
+        Set<Integer> result = assertTimeout(Duration.ofMillis(1100), () ->
+          Stream.generate(() -> supplier(() -> blockingFoo()))
+            .limit(collectionSize)
+            .collect(toSetInParallel(executor))
+            .join());
+
+        assertThat(result).hasSize(1);
     }
 
     @AfterEach
     void after() {
-        executor.shutdown();
+        if (executor != null) {
+            executor.shutdown();
+        }
     }
 
     private static int blockingFoo() {
