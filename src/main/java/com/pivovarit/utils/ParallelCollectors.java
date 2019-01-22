7a85ad09b7bbdda791412de8df1fb689c88436b1
pivovarit <gpiwowarek@gmail.com>
2019-01-22 13:28:04 +0100

    Add dummy API endpoints

--- src/main/java/com/pivovarit/utils/ParallelCollectors.java
+++ src/main/java/com/pivovarit/utils/ParallelCollectors.java
@@ -1,10 +1,12 @@
 package com.pivovarit.utils;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 public final class ParallelCollectors {
     private ParallelCollectors() {
@@ -16,5 +18,25 @@ public final class ParallelCollectors {
 
     public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> toListInParallel(Executor executor) {
         return new ParallelCollectionCollector<>(executor);
+    }
+
+    public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> toListInParallel(int parallelism, Executor executor) {
+        return null; // TODO
+    }
+
+    public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> toCollectionInParallel(Supplier<Collection> collection, Executor executor) {
+        return null; // TODO
+    }
+
+    public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> toCollectionInParallel(Supplier<Collection> collection, int parallelism, Executor executor) {
+        return null; // TODO
+    }
+
+    public static <T> Collector<Supplier<T>, Stream<CompletableFuture<T>>, CompletableFuture<List<T>>> toStreamInParallel(Executor executor) {
+        return null; // TODO
+    }
+
+    public static <T> Collector<Supplier<T>, Stream<CompletableFuture<T>>, CompletableFuture<List<T>>> toStreamInParallel(int parallelism, Executor executor) {
+        return null; // TODO
     }
 }
