4d7448517ec0b1a7ee0af404bdebe4a12dd39cf0
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-01-22 17:52:17 +0100

    Implement a generic parallel collector (#14)
    * Implement a generic parallel collector
    
    * Make helper method static

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -1,6 +1,9 @@
 package com.pivovarit.collectors;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
@@ -15,6 +18,10 @@ public final class ParallelCollectors {
     }
 
     public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> toListInParallel(Executor executor) {
-        return new ParallelCollectionCollector<>(executor);
+        return new ParallelCollectionCollector<>(executor, ArrayList::new);
+    }
+
+    public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<Set<T>>> toSetInParallel(Executor executor) {
+        return new ParallelCollectionCollector<>(executor, HashSet::new);
     }
 }
