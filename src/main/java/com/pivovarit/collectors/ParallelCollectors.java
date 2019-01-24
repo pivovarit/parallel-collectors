3229dde293a471badf44dc6bb963c7d242e590be
pivovarit <gpiwowarek@gmail.com>
2019-01-24 17:56:08 +0100

    Remove unused import

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
