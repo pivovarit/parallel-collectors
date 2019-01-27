1fcb233b68e8e1d8834d4b943783697de7f1a431
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-01-27 10:28:53 +0100

    Remove redundant implements clauses (#75)

--- src/main/java/com/pivovarit/collectors/UnboundedParallelCollector.java
+++ src/main/java/com/pivovarit/collectors/UnboundedParallelCollector.java
@@ -2,19 +2,15 @@ package com.pivovarit.collectors;
 
 import java.util.Collection;
 import java.util.EnumSet;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
 
 /**
  * @author Grzegorz Piwowarek
  */
-class UnboundedParallelCollector<T, R1, R2 extends Collection<R1>> extends AbstractParallelCollector<T, R1, R2>
-  implements Collector<T, List<CompletableFuture<R1>>, CompletableFuture<R2>> {
+class UnboundedParallelCollector<T, R1, R2 extends Collection<R1>> extends AbstractParallelCollector<T, R1, R2> {
     UnboundedParallelCollector(
       Function<T, R1> operation,
       Supplier<R2> collection,
