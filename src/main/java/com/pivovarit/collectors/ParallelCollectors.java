05c868c5acb23ac2c6ad75d9652a5c470eb0c01c
pivovarit <gpiwowarek@gmail.com>
2019-01-29 01:34:30 +0100

    Remove unbounded collector

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -81,7 +81,7 @@ public final class ParallelCollectors {
     public static <T, C extends Collection<T>> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<C>> inParallelToCollection(Supplier<C> collectionSupplier, Executor executor) {
         requireNonNull(collectionSupplier);
         requireNonNull(executor);
-        return new UnboundedParallelCollector<>(Supplier::get, collectionSupplier, executor);
+        return new ThrottlingParallelCollector<>(Supplier::get, collectionSupplier, executor, Integer.MAX_VALUE);
     }
 
     /**
@@ -149,7 +149,7 @@ public final class ParallelCollectors {
         requireNonNull(collectionSupplier);
         requireNonNull(executor);
         requireNonNull(mapper);
-        return new UnboundedParallelCollector<>(mapper, collectionSupplier, executor);
+        return new ThrottlingParallelCollector<>(mapper, collectionSupplier, executor, Integer.MAX_VALUE);
     }
 
     /**
@@ -214,7 +214,7 @@ public final class ParallelCollectors {
      */
     public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<List<T>>> inParallelToList(Executor executor) {
         requireNonNull(executor);
-        return new UnboundedParallelCollector<>(Supplier::get, ArrayList::new, executor);
+        return new ThrottlingParallelCollector<>(Supplier::get, ArrayList::new, executor, Integer.MAX_VALUE);
     }
 
     /**
@@ -279,7 +279,7 @@ public final class ParallelCollectors {
     public static <T, R> Collector<T, List<CompletableFuture<R>>, CompletableFuture<List<R>>> inParallelToList(Function<T, R> mapper, Executor executor) {
         requireNonNull(executor);
         requireNonNull(mapper);
-        return new UnboundedParallelCollector<>(mapper, ArrayList::new, executor);
+        return new ThrottlingParallelCollector<>(mapper, ArrayList::new, executor, Integer.MAX_VALUE);
     }
 
     /**
@@ -342,7 +342,7 @@ public final class ParallelCollectors {
      */
     public static <T> Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<Set<T>>> inParallelToSet(Executor executor) {
         requireNonNull(executor);
-        return new UnboundedParallelCollector<>(Supplier::get, HashSet::new, executor);
+        return new ThrottlingParallelCollector<>(Supplier::get, HashSet::new, executor, Integer.MAX_VALUE);
     }
 
     /**
@@ -405,7 +405,7 @@ public final class ParallelCollectors {
     public static <T, R> Collector<T, List<CompletableFuture<R>>, CompletableFuture<Set<R>>> inParallelToSet(Function<T, R> mapper, Executor executor) {
         requireNonNull(executor);
         requireNonNull(mapper);
-        return new UnboundedParallelCollector<>(mapper, HashSet::new, executor);
+        return new ThrottlingParallelCollector<>(mapper, HashSet::new, executor, Integer.MAX_VALUE);
     }
 
     /**
