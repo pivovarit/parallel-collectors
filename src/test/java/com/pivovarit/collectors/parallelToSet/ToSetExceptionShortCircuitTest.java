6667d5bc0d71c15b3dd1b4570611668a1a141ea3
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-02-02 08:50:05 +0100

    Improve short-circuiting test assertions (#142)

--- src/test/java/com/pivovarit/collectors/parallelToSet/ToSetExceptionShortCircuitTest.java
+++ src/test/java/com/pivovarit/collectors/parallelToSet/ToSetExceptionShortCircuitTest.java
@@ -4,12 +4,14 @@ import com.pivovarit.collectors.infrastructure.ExecutorAwareTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
 
 import static com.pivovarit.collectors.ParallelCollectors.parallelToSet;
 import static com.pivovarit.collectors.ParallelCollectors.supplier;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ToSetExceptionShortCircuitTest extends ExecutorAwareTest {
 
@@ -24,7 +26,7 @@ class ToSetExceptionShortCircuitTest extends ExecutorAwareTest {
         // given
         LongAdder counter = new LongAdder();
 
-        try {
+        assertThatThrownBy(() -> {
             IntStream.generate(() -> 42).boxed().limit(10)
               .map(i -> supplier(() -> {
                   counter.increment();
@@ -32,8 +34,8 @@ class ToSetExceptionShortCircuitTest extends ExecutorAwareTest {
               }))
               .collect(parallelToSet(executor, 1))
               .join();
-        } catch (Exception e) {
-        }
+        }).isInstanceOf(CompletionException.class)
+          .hasCauseExactlyInstanceOf(IllegalArgumentException.class);
 
         assertThat(counter.longValue()).isOne();
     }
@@ -44,7 +46,7 @@ class ToSetExceptionShortCircuitTest extends ExecutorAwareTest {
         // given
         LongAdder counter = new LongAdder();
 
-        try {
+        assertThatThrownBy(() -> {
             IntStream.generate(() -> 42).boxed().limit(10)
               .map(i -> supplier(() -> {
                   counter.increment();
@@ -52,8 +54,8 @@ class ToSetExceptionShortCircuitTest extends ExecutorAwareTest {
               }))
               .collect(parallelToSet(executor))
               .join();
-        } catch (Exception e) {
-        }
+        }).isInstanceOf(CompletionException.class)
+          .hasCauseExactlyInstanceOf(IllegalArgumentException.class);
 
         assertThat(counter.longValue()).isOne();
     }
