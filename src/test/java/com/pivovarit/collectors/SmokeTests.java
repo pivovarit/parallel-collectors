7f202f123d6fbcf616062dda168c415d9787bfa9
pivovarit <gpiwowarek@gmail.com>
2019-02-03 10:06:32 +0100

    Add prefixes

--- src/test/java/com/pivovarit/collectors/SmokeTests.java
+++ src/test/java/com/pivovarit/collectors/SmokeTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.TestFactory;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -18,6 +17,7 @@ import java.util.stream.Stream;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToCollection;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToList;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToSet;
+import static java.lang.String.format;
 import static java.util.function.Function.identity;
 import static java.util.stream.Stream.of;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,18 +33,18 @@ class SmokeTests {
     @TestFactory
     Stream<DynamicTest> testCollectors() {
         return of(
-          forCollector(parallelToSet(i -> i, executor)),
-          forCollector(parallelToSet(i -> i, executor)),
-          forCollector(parallelToList(i -> i, executor)),
-          forCollector(parallelToList(i -> i, executor, 2)),
-          forCollector(parallelToCollection(i -> i, ArrayList::new, executor)),
-          forCollector(parallelToCollection(i -> i, ArrayList::new, executor, 2))
+          forCollector(parallelToSet(i -> i, executor), "parallelToSet"),
+          forCollector(parallelToSet(i -> i, executor), "parallelToSet"),
+          forCollector(parallelToList(i -> i, executor), "parallelToList"),
+          forCollector(parallelToList(i -> i, executor, 2), "parallelToList"),
+          forCollector(parallelToCollection(i -> i, ArrayList::new, executor), "parallelToCollection"),
+          forCollector(parallelToCollection(i -> i, ArrayList::new, executor, 2), "parallelToCollection")
         ).flatMap(identity());
     }
 
-    private <T, R extends Collection<T>> Stream<DynamicTest> forCollector(Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>> collector) {
+    private <T, R extends Collection<T>> Stream<DynamicTest> forCollector(Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>> collector, String name) {
         return of(
-          dynamicTest("parallelToCollection should collect", () -> {
+          dynamicTest(format("%s: should collect", name), () -> {
               List<T> elements = (List<T>) IntStream.range(0, 10).boxed().collect(Collectors.toList());
               Collection<T> result = elements.stream().collect(collector).join();
 
@@ -52,7 +52,7 @@ class SmokeTests {
                 .hasSameSizeAs(elements)
                 .containsOnlyElementsOf(elements);
           }),
-          dynamicTest("parallelToCollection should collect to empty", () -> {
+          dynamicTest(format("%s: should collect to empty", name), () -> {
               List<T> elements = (List<T>) IntStream.of().boxed().collect(Collectors.toList());
               Collection<T> result11 = elements.stream().collect(collector).join();
 
