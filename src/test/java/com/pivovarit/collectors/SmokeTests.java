7cd7dc02b4e30d31b9503874220e3dced14313e1
pivovarit <gpiwowarek@gmail.com>
2019-02-03 10:01:45 +0100

    Remove redundant tests

--- src/test/java/com/pivovarit/collectors/SmokeTests.java
+++ src/test/java/com/pivovarit/collectors/SmokeTests.java
@@ -33,63 +33,28 @@ class SmokeTests {
     @TestFactory
     Stream<DynamicTest> testCollectors() {
         return of(
-          forListCollector(parallelToList(i -> i, executor)),
-          forListCollector(parallelToList(i -> i, executor, 2)),
-          forSetCollector(parallelToSet(i -> i, executor)),
-          forSetCollector(parallelToSet(i -> i, executor, 2)),
-          forCollectionCollector(parallelToCollection(i -> i, ArrayList::new, executor)),
-          forCollectionCollector(parallelToCollection(i -> i, ArrayList::new, executor, 2))
+          forCollector(parallelToSet(i -> i, executor)),
+          forCollector(parallelToSet(i -> i, executor)),
+          forCollector(parallelToList(i -> i, executor)),
+          forCollector(parallelToList(i -> i, executor, 2)),
+          forCollector(parallelToCollection(i -> i, ArrayList::new, executor)),
+          forCollector(parallelToCollection(i -> i, ArrayList::new, executor, 2))
         ).flatMap(identity());
     }
 
-    private Stream<DynamicTest> forCollectionCollector(Collector<Integer, List<CompletableFuture<Integer>>, CompletableFuture<Collection<Integer>>> collector) {
+    private <T, R extends Collection<T>> Stream<DynamicTest> forCollector(Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>> collector) {
         return of(
           dynamicTest("parallelToCollection should collect", () -> {
-              List<Integer> elements = IntStream.range(0, 10).boxed().collect(Collectors.toList());
-              Collection<Integer> result = elements.stream().collect(collector).join();
+              List<T> elements = (List<T>) IntStream.range(0, 10).boxed().collect(Collectors.toList());
+              Collection<T> result = elements.stream().collect(collector).join();
 
               assertThat(result)
                 .hasSameSizeAs(elements)
                 .containsOnlyElementsOf(elements);
           }),
           dynamicTest("parallelToCollection should collect to empty", () -> {
-              Collection<Integer> result11 = Stream.<Integer>of().collect(collector).join();
-
-              assertThat(result11)
-                .isEmpty();
-          }));
-    }
-
-    private Stream<DynamicTest> forSetCollector(Collector<Integer, List<CompletableFuture<Integer>>, CompletableFuture<Set<Integer>>> collector) {
-        return of(
-          dynamicTest("parallelToSet should collect", () -> {
-              List<Integer> elements = IntStream.generate(() -> 42).limit(100).boxed().collect(Collectors.toList());
-              Collection<Integer> result = elements.stream().collect(collector).join();
-
-              assertThat(result)
-                .hasSize(1)
-                .contains(42);
-          }),
-          dynamicTest("parallelToSet should collect to empty", () -> {
-              Collection<Integer> result11 = Stream.<Integer>of().collect(collector).join();
-
-              assertThat(result11)
-                .isEmpty();
-          }));
-    }
-
-    private Stream<DynamicTest> forListCollector(Collector<Integer, List<CompletableFuture<Integer>>, CompletableFuture<List<Integer>>> collector) {
-        return of(
-          dynamicTest("parallelToList should collect", () -> {
-              List<Integer> elements = IntStream.range(0, 10).boxed().collect(Collectors.toList());
-              Collection<Integer> result = elements.stream().collect(collector).join();
-
-              assertThat(result)
-                .hasSameSizeAs(elements)
-                .containsOnlyElementsOf(elements);
-          }),
-          dynamicTest("parallelToList should collect to empty", () -> {
-              Collection<Integer> result11 = Stream.<Integer>of().collect(collector).join();
+              List<T> elements = (List<T>) IntStream.of().boxed().collect(Collectors.toList());
+              Collection<T> result11 = elements.stream().collect(collector).join();
 
               assertThat(result11)
                 .isEmpty();
