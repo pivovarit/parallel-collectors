0cbfe13fe3d361ac62ec24bee00c4bba59be8ed1
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-02-03 10:22:29 +0100

    Remove redundant test cases (#157)

--- src/test/java/com/pivovarit/collectors/SmokeTests.java
+++ src/test/java/com/pivovarit/collectors/SmokeTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.TestFactory;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -18,6 +17,7 @@ import java.util.stream.Stream;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToCollection;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToList;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToSet;
+import static java.lang.String.format;
 import static java.util.function.Function.identity;
 import static java.util.stream.Stream.of;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,66 +33,39 @@ class SmokeTests {
     @TestFactory
     Stream<DynamicTest> testCollectors() {
         return of(
-          forListCollector(parallelToList(i -> i, executor)),
-          forListCollector(parallelToList(i -> i, executor, 2)),
-          forSetCollector(parallelToSet(i -> i, executor)),
-          forSetCollector(parallelToSet(i -> i, executor, 2)),
-          forCollectionCollector(parallelToCollection(i -> i, ArrayList::new, executor)),
-          forCollectionCollector(parallelToCollection(i -> i, ArrayList::new, executor, 2))
+          forCollector(parallelToSet(i -> i, executor), "parallelToSet(p=inf)"),
+          forCollector(parallelToSet(i -> i, executor, 10), "parallelToSet(p=10)"),
+          forCollector(parallelToList(i -> i, executor), "parallelToList(p=inf)"),
+          forCollector(parallelToList(i -> i, executor, 10), "parallelToList(p=10)"),
+          forCollector(parallelToCollection(i -> i, ArrayList::new, executor), "parallelToCollection(p=inf)"),
+          forCollector(parallelToCollection(i -> i, ArrayList::new, executor, 10), "parallelToCollection(p=10)")
         ).flatMap(identity());
     }
 
-    private Stream<DynamicTest> forCollectionCollector(Collector<Integer, List<CompletableFuture<Integer>>, CompletableFuture<Collection<Integer>>> collector) {
+    private static <T, R extends Collection<T>> Stream<DynamicTest> forCollector(Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>> collector, String name) {
         return of(
-          dynamicTest("parallelToCollection should collect", () -> {
-              List<Integer> elements = IntStream.range(0, 10).boxed().collect(Collectors.toList());
-              Collection<Integer> result = elements.stream().collect(collector).join();
-
-              assertThat(result)
-                .hasSameSizeAs(elements)
-                .containsOnlyElementsOf(elements);
-          }),
-          dynamicTest("parallelToCollection should collect to empty", () -> {
-              Collection<Integer> result11 = Stream.<Integer>of().collect(collector).join();
-
-              assertThat(result11)
-                .isEmpty();
-          }));
+          shouldCollect(collector, name),
+          shouldCollectToEmpty(collector, name));
     }
 
-    private Stream<DynamicTest> forSetCollector(Collector<Integer, List<CompletableFuture<Integer>>, CompletableFuture<Set<Integer>>> collector) {
-        return of(
-          dynamicTest("parallelToSet should collect", () -> {
-              List<Integer> elements = IntStream.generate(() -> 42).limit(100).boxed().collect(Collectors.toList());
-              Collection<Integer> result = elements.stream().collect(collector).join();
+    private static <T, R extends Collection<T>> DynamicTest shouldCollectToEmpty(Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>> collector, String name) {
+        return dynamicTest(format("%s: should collect to empty", name), () -> {
+            List<T> elements = (List<T>) IntStream.of().boxed().collect(Collectors.toList());
+            Collection<T> result11 = elements.stream().collect(collector).join();
 
-              assertThat(result)
-                .hasSize(1)
-                .contains(42);
-          }),
-          dynamicTest("parallelToSet should collect to empty", () -> {
-              Collection<Integer> result11 = Stream.<Integer>of().collect(collector).join();
-
-              assertThat(result11)
-                .isEmpty();
-          }));
+            assertThat(result11)
+              .isEmpty();
+        });
     }
 
-    private Stream<DynamicTest> forListCollector(Collector<Integer, List<CompletableFuture<Integer>>, CompletableFuture<List<Integer>>> collector) {
-        return of(
-          dynamicTest("parallelToList should collect", () -> {
-              List<Integer> elements = IntStream.range(0, 10).boxed().collect(Collectors.toList());
-              Collection<Integer> result = elements.stream().collect(collector).join();
+    private static <T, R extends Collection<T>> DynamicTest shouldCollect(Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>> collector, String name) {
+        return dynamicTest(format("%s: should collect", name), () -> {
+            List<T> elements = (List<T>) IntStream.range(0, 10).boxed().collect(Collectors.toList());
+            Collection<T> result = elements.stream().collect(collector).join();
 
-              assertThat(result)
-                .hasSameSizeAs(elements)
-                .containsOnlyElementsOf(elements);
-          }),
-          dynamicTest("parallelToList should collect to empty", () -> {
-              Collection<Integer> result11 = Stream.<Integer>of().collect(collector).join();
-
-              assertThat(result11)
-                .isEmpty();
-          }));
+            assertThat(result)
+              .hasSameSizeAs(elements)
+              .containsOnlyElementsOf(elements);
+        });
     }
 }
