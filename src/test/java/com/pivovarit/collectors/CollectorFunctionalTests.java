ea1acf0220e7b68bfe9afec1add2ed9817f51467
pivovarit <gpiwowarek@gmail.com>
2019-02-03 20:59:42 +0100

    Add functional tests for Supplier<T> collectors

--- src/test/java/com/pivovarit/collectors/CollectorFunctionalTests.java
+++ src/test/java/com/pivovarit/collectors/CollectorFunctionalTests.java
@@ -9,8 +9,8 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -19,7 +19,6 @@ import java.util.stream.Stream;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToCollection;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToList;
 import static com.pivovarit.collectors.ParallelCollectors.parallelToSet;
-import static com.pivovarit.collectors.ParallelCollectors.supplier;
 import static com.pivovarit.collectors.infrastructure.TestUtils.returnWithDelay;
 import static java.lang.String.format;
 import static java.time.Duration.ofMillis;
@@ -32,23 +31,23 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 /**
  * @author Grzegorz Piwowarek
  */
-class MappingCollectorFunctionalTests {
+class CollectorFunctionalTests {
 
     private static final Executor executor = Executors.newFixedThreadPool(100);
 
     @TestFactory
-    Stream<DynamicTest> testMappingCollectors() {
+    Stream<DynamicTest> testCollectors() {
         return of(
-          forCollector(e -> parallelToSet(e), "parallelToSet(p=inf)"),
-          forCollector(e -> parallelToSet(e, 10), "parallelToSet(p=10)"),
-          forCollector(e -> parallelToList(e), "parallelToList(p=inf)"),
-          forCollector(e -> parallelToList(e, 10), "parallelToList(p=10)"),
-          forCollector(e -> parallelToCollection(ArrayList::new, e), "parallelToCollection(p=inf)"),
-          forCollector(e -> parallelToCollection(ArrayList::new, e, 10), "parallelToCollection(p=10)")
+          forCollector((mapper, e) -> parallelToSet(mapper, e), "parallelToSet(p=inf)"),
+          forCollector((mapper, e) -> parallelToSet(mapper, e, 10), "parallelToSet(p=10)"),
+          forCollector((mapper, e) -> parallelToList(mapper, e), "parallelToList(p=inf)"),
+          forCollector((mapper, e) -> parallelToList(mapper, e, 10), "parallelToList(p=10)"),
+          forCollector((mapper, e) -> parallelToCollection(mapper, ArrayList::new, e), "parallelToCollection(p=inf)"),
+          forCollector((mapper, e) -> parallelToCollection(mapper, ArrayList::new, e, 10), "parallelToCollection(p=10)")
         ).flatMap(identity());
     }
 
-    private static <T, R extends Collection<T>> Stream<DynamicTest> forCollector(Function<Executor, Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
+    private static <T, R extends Collection<T>> Stream<DynamicTest> forCollector(BiFunction<Function<T, T>, Executor, Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
         return of(
           shouldCollect(collector, name),
           shouldCollectToEmpty(collector, name),
@@ -56,24 +55,21 @@ class MappingCollectorFunctionalTests {
     }
 
     //@Test
-    private static <T, R extends Collection<T>> DynamicTest shouldNotBlockWhenReturningFuture(Function<Executor, Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
+    private static <T, R extends Collection<T>> DynamicTest shouldNotBlockWhenReturningFuture(BiFunction<Function<T, T>, Executor, Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
         return dynamicTest(format("%s: should not block when returning future", name), () -> {
             List<T> elements = (List<T>) IntStream.of().boxed().collect(Collectors.toList());
             assertTimeoutPreemptively(ofMillis(100), () ->
               elements.stream()
                 .limit(5)
-                .map(i -> supplier(() -> (T) returnWithDelay(42L, ofMillis(Integer.MAX_VALUE))))
-                .collect(collector.apply(executor)));
+                .collect(collector.apply(i -> (T) returnWithDelay(42L, ofMillis(Integer.MAX_VALUE)), executor)));
         });
     }
 
     //@Test
-    private static <T, R extends Collection<T>> DynamicTest shouldCollectToEmpty(Function<Executor, Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
+    private static <T, R extends Collection<T>> DynamicTest shouldCollectToEmpty(BiFunction<Function<T, T>, Executor, Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
         return dynamicTest(format("%s: should collect to empty", name), () -> {
             List<T> elements = (List<T>) IntStream.of().boxed().collect(Collectors.toList());
-            Collection<T> result11 = elements.stream()
-              .map(i -> supplier(() -> i))
-              .collect(collector.apply(executor)).join();
+            Collection<T> result11 = elements.stream().collect(collector.apply(i -> i, executor)).join();
 
             assertThat(result11)
               .isEmpty();
@@ -81,12 +77,10 @@ class MappingCollectorFunctionalTests {
     }
 
     //@Test
-    private static <T, R extends Collection<T>> DynamicTest shouldCollect(Function<Executor, Collector<Supplier<T>, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
+    private static <T, R extends Collection<T>> DynamicTest shouldCollect(BiFunction<Function<T, T>, Executor, Collector<T, List<CompletableFuture<T>>, CompletableFuture<R>>> collector, String name) {
         return dynamicTest(format("%s: should collect", name), () -> {
             List<T> elements = (List<T>) IntStream.range(0, 10).boxed().collect(Collectors.toList());
-            Collection<T> result = elements.stream()
-              .map(i -> supplier(() -> i))
-              .collect(collector.apply(executor)).join();
+            Collection<T> result = elements.stream().collect(collector.apply(i -> i, executor)).join();
 
             assertThat(result)
               .hasSameSizeAs(elements)
