38522695ad1b37629459c1ba610cbe51a0e72efc
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-03-18 06:34:04 +0100

    Add package-info (#198)

---
+++ src/main/java/com/pivovarit/collectors/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The {@link com.pivovarit.collectors} package contains all internal components backing the {@link com.pivovarit.collectors.ParallelCollectors} APIs
+ */
+package com.pivovarit.collectors;
+
