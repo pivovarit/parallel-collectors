0117f89dcb14930fa9b421958bd5faade4c84198
pivovarit <gpiwowarek@gmail.com>
2019-03-25 12:58:07 +0100

    Simplify fold-left-futures

--- src/main/java/com/pivovarit/collectors/AbstractParallelCollector.java
+++ src/main/java/com/pivovarit/collectors/AbstractParallelCollector.java
@@ -9,8 +9,10 @@ import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.synchronizedList;
+import static java.util.concurrent.CompletableFuture.allOf;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.stream.Collectors.toCollection;
 
@@ -42,24 +44,9 @@ abstract class AbstractParallelCollector<T, R, C>
     }
 
     static <R, C extends Collection<R>> Function<List<CompletableFuture<R>>, CompletableFuture<C>> foldLeftFutures(Supplier<C> collectionFactory) {
-        return futures -> futures.stream()
-          .reduce(completedFuture(synchronizedList(new ArrayList<>())),
-            accumulatingResults(),
-            mergingPartialResults())
-          .thenApply(list -> list.stream().collect(toCollection(collectionFactory)));
-    }
-
-    static <T1, R1 extends Collection<T1>> BinaryOperator<CompletableFuture<R1>> mergingPartialResults() {
-        return (f1, f2) -> f1.thenCombine(f2, (left, right) -> {
-            left.addAll(right);
-            return left;
-        });
-    }
-
-    static <T1, R1 extends Collection<T1>> BiFunction<CompletableFuture<R1>, CompletableFuture<T1>, CompletableFuture<R1>> accumulatingResults() {
-        return (list, object) -> list.thenCombine(object, (left, right) -> {
-            left.add(right);
-            return left;
-        });
+        return futures -> allOf(futures.toArray(new CompletableFuture<?>[0]))
+          .thenApply(__ -> futures.stream()
+            .map(CompletableFuture::join)
+            .collect(toCollection(collectionFactory)));
     }
 }
