38657c23f0c2e4d82147189f2cb939fbe648201a
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-03-29 20:31:57 +0100

    Add additional toMap tests (#218)
    * Add ToMap tests
    
    * Add missing tests

--- src/test/java/com/pivovarit/collectors/parallelToMap/ToMapParallelismThrottlingBDDTest.java
+++ src/test/java/com/pivovarit/collectors/parallelToMap/ToMapParallelismThrottlingBDDTest.java
@@ -1,4 +1,4 @@
-package com.pivovarit.collectors.parallelToListOrdered;
+package com.pivovarit.collectors.parallelToMap;
 
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.InRange;
@@ -7,21 +7,19 @@ import com.pivovarit.collectors.infrastructure.ExecutorAwareTest;
 import org.junit.runner.RunWith;
 
 import java.time.Duration;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 
-import static com.pivovarit.collectors.ParallelCollectors.parallelToListOrdered;
+import static com.pivovarit.collectors.ParallelCollectors.parallelToMap;
 import static com.pivovarit.collectors.infrastructure.TestUtils.TRIALS;
 import static com.pivovarit.collectors.infrastructure.TestUtils.expectedDuration;
 import static com.pivovarit.collectors.infrastructure.TestUtils.returnWithDelay;
-import static com.pivovarit.collectors.infrastructure.TestUtils.returnWithDelayGaussian;
 import static com.pivovarit.collectors.infrastructure.TestUtils.timed;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.Offset.offset;
@@ -30,18 +28,18 @@ import static org.assertj.core.data.Offset.offset;
  * @author Grzegorz Piwowarek
  */
 @RunWith(JUnitQuickcheck.class)
-public class ToListOrderedParallelismThrottlingBDDTest extends ExecutorAwareTest {
+public class ToMapParallelismThrottlingBDDTest extends ExecutorAwareTest {
 
     private static final long BLOCKING_MILLIS = 50;
     private static final long CONSTANT_DELAY = 100;
 
     @Property(trials = TRIALS)
-    public void shouldCollectToListOrderedWithThrottledParallelism(@InRange(minInt = 2, maxInt = 20) int unitsOfWork, @InRange(minInt = 1, maxInt = 40) int parallelism) {
+    public void shouldCollectToMapWithThrottledParallelism(@InRange(minInt = 2, maxInt = 20) int unitsOfWork, @InRange(minInt = 1, maxInt = 40) int parallelism) {
         // given
         executor = threadPoolExecutor(unitsOfWork);
         long expectedDuration = expectedDuration(parallelism, unitsOfWork, BLOCKING_MILLIS);
 
-        Map.Entry<List<Long>, Long> result = timed(collectWith(f -> parallelToListOrdered(f, executor, parallelism), unitsOfWork));
+        Map.Entry<Map<Long, Long>, Long> result = timed(collectWith(f -> parallelToMap(f, i -> ThreadLocalRandom.current().nextLong(), executor, parallelism), unitsOfWork));
 
         assertThat(result)
           .satisfies(e -> {
@@ -53,21 +51,10 @@ public class ToListOrderedParallelismThrottlingBDDTest extends ExecutorAwareTest
           });
     }
 
-    @Property
-    public void shouldMaintainOrder(@InRange(minInt = 2, maxInt = 20) int unitsOfWork, @InRange(minInt = 2, maxInt = 40) int parallelism) {
-        // given
-        executor = threadPoolExecutor(unitsOfWork);
-        List<Integer> result = Stream.iterate(0, i -> i + 1).limit(20)
-          .collect(parallelToListOrdered(i -> returnWithDelayGaussian(i, Duration.ofMillis(10)), executor, parallelism))
-          .join();
-
-        assertThat(result).isSorted();
-    }
-
-    private static <R extends Collection<Long>> Supplier<R> collectWith(Function<UnaryOperator<Long>, Collector<Long, ?, CompletableFuture<R>>> collector, int unitsOfWork) {
+    private static <R extends Map<Long, Long>> Supplier<R> collectWith(Function<UnaryOperator<Long>, Collector<Long, ?, CompletableFuture<R>>> collector, int unitsOfWork) {
         return () -> Stream.generate(() -> 42L)
           .limit(unitsOfWork)
-          .collect(collector.apply(f -> returnWithDelay(42L, Duration.ofMillis(BLOCKING_MILLIS))))
+          .collect(collector.apply(f -> returnWithDelay(ThreadLocalRandom.current().nextLong(), Duration.ofMillis(BLOCKING_MILLIS))))
           .join();
     }
 }
