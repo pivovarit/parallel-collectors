16bf7abf406632ef1aaa18249e8dc0e7b9ae24ac
pivovarit <gpiwowarek@gmail.com>
2019-03-29 19:22:25 +0100

    Add ToMap tests

---
+++ src/test/java/com/pivovarit/collectors/parallelToMap/ToMapParallelismThrottlingTest.java
@@ -0,0 +1,62 @@
+package com.pivovarit.collectors.parallelToMap;
+
+import com.pivovarit.collectors.infrastructure.TestUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import static com.pivovarit.collectors.ParallelCollectors.parallelToList;
+import static com.pivovarit.collectors.ParallelCollectors.parallelToMap;
+import static com.pivovarit.collectors.ParallelCollectors.supplier;
+import static com.pivovarit.collectors.infrastructure.TestUtils.returnWithDelay;
+import static java.time.Duration.ofMillis;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Grzegorz Piwowarek
+ */
+class ToMapParallelismThrottlingTest {
+
+    @Test
+    void shouldParallelizeToMapAndRespectParallelizm() throws InterruptedException {
+        // given
+        int parallelism = 2;
+        TestUtils.CountingExecutor executor = new TestUtils.CountingExecutor();
+
+        CompletableFuture<Map<Long, Long>> result =
+          Stream.generate(() -> 42)
+            .limit(10)
+            .collect(parallelToMap(i -> returnWithDelay(42L, ofMillis(Integer.MAX_VALUE)), i -> returnWithDelay(42L, ofMillis(Integer.MAX_VALUE)), executor, parallelism));
+
+        assertThat(result)
+          .isNotCompleted()
+          .isNotCancelled();
+
+        Thread.sleep(50);
+        assertThat(executor.count()).isEqualTo(parallelism);
+    }
+
+
+    @Test
+    void shouldParallelizeToMapAndRespectParallelizmMapping() throws InterruptedException {
+        // given
+        int parallelism = 2;
+        TestUtils.CountingExecutor executor = new TestUtils.CountingExecutor();
+
+
+        CompletableFuture<Map<Long, Long>> result =
+          Stream.generate(() -> 42)
+            .limit(10)
+            .collect(parallelToMap(i -> returnWithDelay(42L, ofMillis(Integer.MAX_VALUE)),i -> returnWithDelay(42L, ofMillis(Integer.MAX_VALUE)) , executor, parallelism));
+
+        assertThat(result)
+          .isNotCompleted()
+          .isNotCancelled();
+
+        Thread.sleep(50);
+        assertThat(executor.count()).isEqualTo(parallelism);
+    }
+}
