ab53fa0f58ab81638e2725e6517918b6ed665dae
pivovarit <gpiwowarek@gmail.com>
2019-03-31 12:42:00 +0200

    Extract AbstractAsynUnorderedParallelCollector

---
+++ src/main/java/com/pivovarit/collectors/AbstractAsyncUnorderedParallelCollector.java
@@ -0,0 +1,69 @@
+package com.pivovarit.collectors;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.concurrent.CompletableFuture.allOf;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+/**
+ * @author Grzegorz Piwowarek
+ */
+abstract class AbstractAsyncUnorderedParallelCollector<T, R, C>
+  extends AbstractAsyncCollector<T, R, C>
+  implements AutoCloseable {
+
+    private final Dispatcher<R> dispatcher;
+    private final Function<T, R> function;
+
+    AbstractAsyncUnorderedParallelCollector(
+      Function<T, R> function,
+      Executor executor,
+      int parallelism) {
+        this.dispatcher = new ThrottlingDispatcher<>(executor, parallelism);
+        this.function = function;
+    }
+
+    AbstractAsyncUnorderedParallelCollector(
+      Function<T, R> function,
+      Executor executor) {
+        this.dispatcher = new UnboundedDispatcher<>(executor);
+        this.function = function;
+    }
+
+    abstract Function<CompletableFuture<Stream<R>>, CompletableFuture<C>> resultsProcessor();
+
+    @Override
+    public BiConsumer<List<CompletableFuture<R>>, T> accumulator() {
+        return (acc, e) -> acc.add(dispatcher.enqueue(() -> function.apply(e)));
+    }
+
+    @Override
+    public Function<List<CompletableFuture<R>>, CompletableFuture<C>> finisher() {
+        if (!dispatcher.isEmpty()) {
+            dispatcher.start();
+            return futures -> resultsProcessor().andThen(f -> supplyWithResources(() -> f, dispatcher::close))
+              .apply(allOf(futures.toArray(new CompletableFuture<?>[0]))
+                .thenApply(__ -> futures.stream()
+                  .map(CompletableFuture::join)));
+        } else {
+            return futures -> resultsProcessor().apply(completedFuture(Stream.empty()));
+        }
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return EnumSet.of(Characteristics.UNORDERED);
+    }
+
+    @Override
+    public void close() {
+        dispatcher.close();
+    }
+}
--- src/main/java/com/pivovarit/collectors/AsyncDelegatingParallelCollector.java
+++ src/main/java/com/pivovarit/collectors/AsyncDelegatingParallelCollector.java
@@ -1,26 +1,17 @@
 package com.pivovarit.collectors;
 
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collector;
-
-import static java.util.concurrent.CompletableFuture.allOf;
+import java.util.stream.Stream;
 
 /**
  * @author Grzegorz Piwowarek
  */
 final class AsyncDelegatingParallelCollector<T, R, C>
-  extends AbstractAsyncCollector<T, R, C>
+  extends AbstractAsyncUnorderedParallelCollector<T, R, C>
   implements AutoCloseable {
-
-    private final Dispatcher<R> dispatcher;
-
-    private final Function<T, R> function;
 
     private final Collector<R, ?, C> collector;
 
@@ -29,48 +20,20 @@ final class AsyncDelegatingParallelCollector<T, R, C>
       Collector<R, ?, C> collector,
       Executor executor,
       int parallelism) {
-        this.dispatcher = new ThrottlingDispatcher<>(executor, parallelism);
+        super(function, executor, parallelism);
         this.collector = collector;
-        this.function = function;
     }
 
     AsyncDelegatingParallelCollector(
       Function<T, R> function,
       Collector<R, ?, C> collector,
       Executor executor) {
-        this.dispatcher = new UnboundedDispatcher<>(executor);
+        super(function, executor);
         this.collector = collector;
-        this.function = function;
     }
 
     @Override
-    public BiConsumer<List<CompletableFuture<R>>, T> accumulator() {
-        return (acc, e) -> acc.add(dispatcher.enqueue(() -> function.apply(e)));
-    }
-
-    @Override
-    public Function<List<CompletableFuture<R>>, CompletableFuture<C>> finisher() {
-        if (!dispatcher.isEmpty()) {
-            dispatcher.start();
-        }
-
-        return collectingWith(collector).andThen(f -> supplyWithResources(() -> f, dispatcher::close));
-    }
-
-    private Function<List<CompletableFuture<R>>, CompletableFuture<C>> collectingWith(Collector<R, ?, C> collector) {
-        return futures -> allOf(futures.toArray(new CompletableFuture<?>[0]))
-          .thenApply(__ -> futures.stream()
-            .map(CompletableFuture::join)
-            .collect(collector));
-    }
-
-    @Override
-    public Set<Characteristics> characteristics() {
-        return EnumSet.of(Characteristics.UNORDERED);
-    }
-
-    @Override
-    public void close() {
-        dispatcher.close();
+    Function<CompletableFuture<Stream<R>>, CompletableFuture<C>> resultsProcessor() {
+        return result -> result.thenApply(futures -> futures.collect(collector));
     }
 }
