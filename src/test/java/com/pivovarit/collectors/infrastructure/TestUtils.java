8b2a7f409981e64ac61f4876f517369cab900f75
pivovarit <gpiwowarek@gmail.com>
2019-04-20 11:09:24 +0200

    A single trial

--- src/test/java/com/pivovarit/collectors/infrastructure/TestUtils.java
+++ src/test/java/com/pivovarit/collectors/infrastructure/TestUtils.java
@@ -19,7 +19,7 @@ public final class TestUtils {
     private TestUtils() {
     }
 
-    public static final int TRIALS = 10;
+    public static final int TRIALS = 1;
 
     public static <T> Map.Entry<T, Long> timed(Supplier<T> runnable) {
         Instant start = Instant.now();
