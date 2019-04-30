8d05285d8ec4008bdfbbfed26a7635154d2a34f6
pivovarit <gpiwowarek@gmail.com>
2019-04-30 20:56:02 +0200

    Update documentation

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -554,7 +554,7 @@ public final class ParallelCollectors {
      *
      * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
      *
-     * @since 0.4.0
+     * @since 1.0.0
      */
     public static <T, R> Collector<T, ?, Stream<R>> parallelMap(Function<T, R> mapper, Executor executor) {
         return ParallelStreamCollector.streaming(mapper, executor);
@@ -580,7 +580,7 @@ public final class ParallelCollectors {
      *
      * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
      *
-     * @since 0.4.0
+     * @since 1.0.0
      */
     public static <T, R> Collector<T, ?, Stream<R>> parallelMap(Function<T, R> mapper, Executor executor, int parallelism) {
         return ParallelStreamCollector.streaming(mapper, executor, parallelism);
@@ -611,7 +611,7 @@ public final class ParallelCollectors {
      *
      * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
      *
-     * @since 0.4.0
+     * @since 1.0.0
      */
     public static <T, R> Collector<T, ?, Stream<R>> parallelMapOrdered(Function<T, R> mapper, Executor executor) {
         return ParallelStreamCollector.streamingOrdered(mapper, executor);
@@ -637,7 +637,7 @@ public final class ParallelCollectors {
      *
      * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
      *
-     * @since 0.4.0
+     * @since 1.0.0
      */
     public static <T, R> Collector<T, ?, Stream<R>> parallelMapOrdered(Function<T, R> mapper, Executor executor, int parallelism) {
         return ParallelStreamCollector.streamingOrdered(mapper, executor, parallelism);
