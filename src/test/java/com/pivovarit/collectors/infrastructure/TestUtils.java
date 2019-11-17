e8989f193f7c3bf0bece9a08c9ecf4cc7b34b2a6
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-11-17 18:56:26 +0100

    Remove unused

--- src/test/java/com/pivovarit/collectors/infrastructure/TestUtils.java
+++ src/test/java/com/pivovarit/collectors/infrastructure/TestUtils.java
@@ -15,18 +15,6 @@ public final class TestUtils {
     private TestUtils() {
     }
 
-    public static final int TRIALS = 5;
-
-    public static <T> T returnWithDelayGaussian(T value, Duration duration) {
-        try {
-            Thread.sleep(Math.abs((long) (duration.toMillis() * new Random().nextGaussian())));
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        return value;
-    }
-
     public static <T> T returnWithDelay(T value, Duration duration) {
         try {
             Thread.sleep(duration.toMillis());
@@ -46,20 +34,6 @@ public final class TestUtils {
         counter.increment();
         throw new IllegalArgumentException();
     }
-
-    public static class CountingExecutor implements Executor {
-        private final LongAdder longAdder = new LongAdder();
-
-        @Override
-        public void execute(Runnable command) {
-            longAdder.increment();
-        }
-
-        public long count() {
-            return longAdder.longValue();
-        }
-    }
-
 
     public static void runWithExecutor(Consumer<Executor> consumer, int size) {
         ExecutorService executor = Executors.newFixedThreadPool(size);
