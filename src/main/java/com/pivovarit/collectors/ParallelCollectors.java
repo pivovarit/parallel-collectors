4b74f12e44c28ab05af0980ab481a6cb636058c4
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-05 11:00:15 +0700

    Remove redundant collectors

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -254,36 +254,6 @@ public final class ParallelCollectors {
          *
          * @implNote this collector processed elements in batches and not separately
          *
-         * <br><br>
-         * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
-         *
-         * <br>
-         * Example:
-         * <pre>{@code
-         * CompletableFuture<List<String>> result = Stream.of(1, 2, 3)
-         *   .collect(parallel(i -> foo(i), toList(), executor));
-         * }</pre>
-         *
-         * @param mapper    a transformation to be performed in parallel
-         * @param collector the {@code Collector} describing the reduction
-         * @param executor  the {@code Executor} to use for asynchronous execution
-         * @param <T>       the type of the collected elements
-         * @param <R>       the result returned by {@code mapper}
-         *
-         * @return a {@code Collector} which collects all processed elements into a user-provided mutable {@code Collection} in parallel
-         *
-         * @since 2.1.0
-         */
-        public static <T, R, RR> Collector<T, ?, CompletableFuture<RR>> parallel(Function<T, R> mapper, Collector<R, ?, RR> collector, Executor executor) {
-            return AsyncParallelCollector.collectingWithCollectorInBatches(collector, mapper, executor);
-        }
-
-        /**
-         * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
-         * and returning them as a {@link CompletableFuture} containing a result of the application of the user-provided {@link Collector}.
-         *
-         * @implNote this collector processed elements in batches and not separately
-         *
          * <br>
          * Example:
          * <pre>{@code
@@ -304,38 +274,6 @@ public final class ParallelCollectors {
          */
         public static <T, R, RR> Collector<T, ?, CompletableFuture<RR>> parallel(Function<T, R> mapper, Collector<R, ?, RR> collector, Executor executor, int parallelism) {
             return AsyncParallelCollector.collectingWithCollectorInBatches(collector, mapper, executor, parallelism);
-        }
-
-        /**
-         * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
-         * and returning them as {@link CompletableFuture} containing a {@link Stream} of these elements
-         *
-         * @implNote this collector processed elements in batches and not separately
-         *
-         * <br><br>
-         * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
-         *
-         * <br><br>
-         * The collector maintains the order of processed {@link Stream}. Instances should not be reused.
-         *
-         * <br>
-         * Example:
-         * <pre>{@code
-         * CompletableFuture<Stream<String>> result = Stream.of(1, 2, 3)
-         *   .collect(parallel(i -> foo(), executor));
-         * }</pre>
-         *
-         * @param mapper   a transformation to be performed in parallel
-         * @param executor the {@code Executor} to use for asynchronous execution
-         * @param <T>      the type of the collected elements
-         * @param <R>      the result returned by {@code mapper}
-         *
-         * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
-         *
-         * @since 2.1.0
-         */
-        public static <T, R> Collector<T, ?, CompletableFuture<Stream<R>>> parallel(Function<T, R> mapper, Executor executor) {
-            return AsyncParallelCollector.collectingToStreamInBatches(mapper, executor);
         }
 
         /**
@@ -371,38 +309,6 @@ public final class ParallelCollectors {
             return AsyncParallelCollector.collectingToStreamInBatches(mapper, executor, parallelism);
         }
 
-        /**
-         * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
-         * and returning a {@link Stream} instance returning results in completion order
-         *
-         * @implNote this collector processed elements in batches and not separately
-         *
-         * <br><br>
-         * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
-         *
-         * <br><br>
-         * Instances should not be reused.
-         *
-         * <br>
-         * Example:
-         * <pre>{@code
-         * List<String> result = Stream.of(1, 2, 3)
-         *   .collect(parallelToStream(i -> foo(), executor))
-         *   .collect(toList());
-         * }</pre>
-         *
-         * @param mapper   a transformation to be performed in parallel
-         * @param executor the {@code Executor} to use for asynchronous execution
-         * @param <T>      the type of the collected elements
-         * @param <R>      the result returned by {@code mapper}
-         *
-         * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
-         *
-         * @since 2.1.0
-         */
-        public static <T, R> Collector<T, ?, Stream<R>> parallelToStream(Function<T, R> mapper, Executor executor) {
-            return ParallelStreamCollector.streamingInBatches(mapper, executor);
-        }
 
         /**
          * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
@@ -430,38 +336,6 @@ public final class ParallelCollectors {
          */
         public static <T, R> Collector<T, ?, Stream<R>> parallelToStream(Function<T, R> mapper, Executor executor, int parallelism) {
             return ParallelStreamCollector.streamingInBatches(mapper, executor, parallelism);
-        }
-
-        /**
-         * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
-         * and returning a {@link Stream} instance returning results as they arrive while maintaining the initial order.
-         *
-         * @implNote this collector processed elements in batches and not separately
-         *
-         * <br><br>
-         * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
-         *
-         * <br><br>
-         * Instances should not be reused.
-         * <br><br>
-         * Example:
-         * <pre>{@code
-         * Stream.of(1, 2, 3)
-         *   .collect(parallelToOrderedStream(i -> foo(), executor))
-         *   .forEach(System.out::println);
-         * }</pre>
-         *
-         * @param mapper   a transformation to be performed in parallel
-         * @param executor the {@code Executor} to use for asynchronous execution
-         * @param <T>      the type of the collected elements
-         * @param <R>      the result returned by {@code mapper}
-         *
-         * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel
-         *
-         * @since 2.1.0
-         */
-        public static <T, R> Collector<T, ?, Stream<R>> parallelToOrderedStream(Function<T, R> mapper, Executor executor) {
-            return ParallelStreamCollector.streamingOrderedInBatches(mapper, executor);
         }
 
         /**
