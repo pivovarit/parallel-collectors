1679a2a832622becdc53483112080d5661ddbafb
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-01-05 06:47:38 +0200

    Mention 'max parallelism' explicitly

--- src/main/java/com/pivovarit/collectors/ParallelCollectors.java
+++ src/main/java/com/pivovarit/collectors/ParallelCollectors.java
@@ -23,7 +23,7 @@ public final class ParallelCollectors {
      * and returning them as a {@link CompletableFuture} containing a result of the application of the user-provided {@link Collector}.
      *
      * <br><br>
-     * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
+     * The maximum parallelism level defaults to {@code Runtime.availableProcessors() - 1}
      *
      * <br>
      * Example:
@@ -62,7 +62,7 @@ public final class ParallelCollectors {
      * @param executor    the {@code Executor} to use for asynchronous execution
      * @param <T>         the type of the collected elements
      * @param <R>         the result returned by {@code mapper}
-     * @param parallelism the parallelism level
+     * @param parallelism the max parallelism level
      *
      * @return a {@code Collector} which collects all processed elements into a user-provided mutable {@code Collection} in parallel
      *
@@ -77,7 +77,7 @@ public final class ParallelCollectors {
      * and returning them as {@link CompletableFuture} containing a {@link Stream} of these elements
      *
      * <br><br>
-     * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
+     * The max parallelism level defaults to {@code Runtime.availableProcessors() - 1}
      *
      * <br><br>
      * The collector maintains the order of processed {@link Stream}. Instances should not be reused.
@@ -107,7 +107,7 @@ public final class ParallelCollectors {
      * and returning them as {@link CompletableFuture} containing a {@link Stream} of these elements.
      *
      * <br><br>
-     * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
+     * The max parallelism level defaults to {@code Runtime.availableProcessors() - 1}
      *
      * <br><br>
      * The collector maintains the order of processed {@link Stream}. Instances should not be reused.
@@ -121,7 +121,7 @@ public final class ParallelCollectors {
      *
      * @param mapper      a transformation to be performed in parallel
      * @param executor    the {@code Executor} to use for asynchronous execution
-     * @param parallelism the parallelism level
+     * @param parallelism the max parallelism level
      * @param <T>         the type of the collected elements
      * @param <R>         the result returned by {@code mapper}
      *
@@ -138,7 +138,7 @@ public final class ParallelCollectors {
      * and returning a {@link Stream} instance returning results in completion order
      *
      * <br><br>
-     * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
+     * The max parallelism level defaults to {@code Runtime.availableProcessors() - 1}
      *
      * <br><br>
      * Instances should not be reused.
@@ -178,7 +178,7 @@ public final class ParallelCollectors {
      *
      * @param mapper      a transformation to be performed in parallel
      * @param executor    the {@code Executor} to use for asynchronous execution
-     * @param parallelism the parallelism level
+     * @param parallelism the max parallelism level
      * @param <T>         the type of the collected elements
      * @param <R>         the result returned by {@code mapper}
      *
@@ -195,7 +195,7 @@ public final class ParallelCollectors {
      * and returning a {@link Stream} instance returning results as they arrive while maintaining the initial order.
      *
      * <br><br>
-     * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
+     * The max parallelism level defaults to {@code Runtime.availableProcessors() - 1}
      *
      * <br><br>
      * Instances should not be reused.
@@ -234,7 +234,7 @@ public final class ParallelCollectors {
      *
      * @param mapper      a transformation to be performed in parallel
      * @param executor    the {@code Executor} to use for asynchronous execution
-     * @param parallelism the parallelism level
+     * @param parallelism the max parallelism level
      * @param <T>         the type of the collected elements
      * @param <R>         the result returned by {@code mapper}
      *
@@ -269,7 +269,7 @@ public final class ParallelCollectors {
          * @param executor    the {@code Executor} to use for asynchronous execution
          * @param <T>         the type of the collected elements
          * @param <R>         the result returned by {@code mapper}
-         * @param parallelism the parallelism level
+         * @param parallelism the max parallelism level
          *
          * @return a {@code Collector} which collects all processed elements into a user-provided mutable {@code Collection} in parallel
          *
@@ -286,7 +286,7 @@ public final class ParallelCollectors {
          * @implNote this collector processed elements in batches and not separately
          *
          * <br><br>
-         * The parallelism level defaults to {@code Runtime.availableProcessors() - 1}
+         * The max parallelism level defaults to {@code Runtime.availableProcessors() - 1}
          *
          * <br><br>
          * The collector maintains the order of processed {@link Stream}. Instances should not be reused.
@@ -300,7 +300,7 @@ public final class ParallelCollectors {
          *
          * @param mapper      a transformation to be performed in parallel
          * @param executor    the {@code Executor} to use for asynchronous execution
-         * @param parallelism the parallelism level
+         * @param parallelism the max parallelism level
          * @param <T>         the type of the collected elements
          * @param <R>         the result returned by {@code mapper}
          *
@@ -329,7 +329,7 @@ public final class ParallelCollectors {
          *
          * @param mapper      a transformation to be performed in parallel
          * @param executor    the {@code Executor} to use for asynchronous execution
-         * @param parallelism the parallelism level
+         * @param parallelism the max parallelism level
          * @param <T>         the type of the collected elements
          * @param <R>         the result returned by {@code mapper}
          *
@@ -357,7 +357,7 @@ public final class ParallelCollectors {
          *
          * @param mapper      a transformation to be performed in parallel
          * @param executor    the {@code Executor} to use for asynchronous execution
-         * @param parallelism the parallelism level
+         * @param parallelism the max parallelism level
          * @param <T>         the type of the collected elements
          * @param <R>         the result returned by {@code mapper}
          *
