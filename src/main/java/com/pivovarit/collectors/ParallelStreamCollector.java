abe0d19d3f25c955b69743563c87e1b0aab1c358
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-02-09 18:43:22 +0100

    Fix batching ordered

--- src/main/java/com/pivovarit/collectors/ParallelStreamCollector.java
+++ src/main/java/com/pivovarit/collectors/ParallelStreamCollector.java
@@ -2,7 +2,6 @@ package com.pivovarit.collectors;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
@@ -136,7 +135,7 @@ class ParallelStreamCollector<T, R> implements Collector<T, List<CompletableFutu
 
             return parallelism == 1
               ? syncCollector(mapper)
-              : batchingCollector(mapper, executor, parallelism);
+              : batched(new ParallelStreamCollector<>(batching(mapper), streamInCompletionOrderStrategy(), UNORDERED, of(executor)), parallelism);
         }
 
         static <T, R> Collector<T, ?, Stream<R>> streamingOrdered(Function<T, R> mapper, Executor executor, int parallelism) {
@@ -146,27 +145,14 @@ class ParallelStreamCollector<T, R> implements Collector<T, List<CompletableFutu
 
             return parallelism == 1
               ? syncCollector(mapper)
-              : batchingOrderedCollector(mapper, executor, parallelism);
+              : batched(new ParallelStreamCollector<>(batching(mapper), streamOrderedStrategy(), emptySet(), of(executor)), parallelism);
         }
 
-        private static <T, R> Collector<T, ?, Stream<R>> batchingCollector(Function<T, R> mapper, Executor executor, int parallelism) {
-            return
-              collectingAndThen(
-                toList(),
-                list -> partitioned(list, parallelism)
-                  .collect(collectingAndThen(
-                    new ParallelStreamCollector<>(batching(mapper), streamInCompletionOrderStrategy(), UNORDERED, of(executor)),
-                    s -> s.flatMap(Collection::stream))));
-        }
-
-        private static <T, R> Collector<T, ?, Stream<R>> batchingOrderedCollector(Function<T, R> mapper, Executor executor, int parallelism) {
-            return
-              collectingAndThen(
-                toList(),
-                list -> partitioned(list, parallelism)
-                  .collect(collectingAndThen(
-                    new ParallelStreamCollector<>(batching(mapper), streamOrderedStrategy(), Collections.emptySet(), of(executor)),
-                    s -> s.flatMap(Collection::stream))));
+        private static <T, R> Collector<T, ?, Stream<R>> batched(ParallelStreamCollector<List<T>, List<R>> downstream, int parallelism) {
+            return collectingAndThen(
+              toList(),
+              list -> partitioned(list, parallelism)
+                .collect(collectingAndThen(downstream, s -> s.flatMap(Collection::stream))));
         }
 
         private static <T, R> Collector<T, List<R>, Stream<R>> syncCollector(Function<T, R> mapper) {
