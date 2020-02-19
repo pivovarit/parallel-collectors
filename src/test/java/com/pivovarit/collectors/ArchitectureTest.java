05cf53b18713cd1024ec13db130cc190de259773
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-02-19 23:24:37 +0100

    Refactor ArchitectureTest

--- src/test/java/com/pivovarit/collectors/ArchitectureTest.java
+++ src/test/java/com/pivovarit/collectors/ArchitectureTest.java
@@ -3,7 +3,6 @@ package com.pivovarit.collectors;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOptions;
-import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.core.domain.JavaModifier.*;
@@ -14,26 +13,6 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 class ArchitectureTest {
 
-    private static final ArchRule SINGLE_PACKAGE_RULE = classes()
-      .should().resideInAPackage("com.pivovarit.collectors");
-
-    private static final ArchRule ONE_FACADE_RULE = classes()
-      .that().arePublic()
-      .should().haveSimpleName("ParallelCollectors")
-      .andShould().haveOnlyFinalFields()
-      .andShould().haveOnlyPrivateConstructors()
-      .andShould().haveModifier(FINAL)
-      .andShould().resideInAPackage("com.pivovarit.collectors")
-      .as("all public factory methods should be accessible from the ParallelCollectors class")
-      .because("users of ParallelCollectors should have a single entry point");
-
-    private static final ArchRule ZERO_DEPS_RULE = classes()
-      .that().resideInAPackage("com.pivovarit.collectors")
-      .should()
-      .dependOnClassesThat().resideInAPackage("java..")
-      .as("the library should depend only on core Java classes")
-      .because("users appreciate not experiencing a dependency hell");
-
     private static final JavaClasses classes = new ClassFileImporter()
       .importClasspath(new ImportOptions()
         .with(DO_NOT_INCLUDE_JARS)
@@ -42,16 +21,33 @@ class ArchitectureTest {
 
     @Test
     void shouldHaveSingleFacade() {
-        ONE_FACADE_RULE.check(classes);
+        classes()
+          .that().arePublic()
+          .should().haveSimpleName("ParallelCollectors")
+          .andShould().haveOnlyFinalFields()
+          .andShould().haveOnlyPrivateConstructors()
+          .andShould().haveModifier(FINAL)
+          .andShould().resideInAPackage("com.pivovarit.collectors")
+          .as("all public factory methods should be accessible from the ParallelCollectors class")
+          .because("users of ParallelCollectors should have a single entry point")
+          .check(classes);
     }
 
     @Test
     void shouldHaveZeroDependencies() {
-        ZERO_DEPS_RULE.check(classes);
+        classes()
+          .that().resideInAPackage("com.pivovarit.collectors")
+          .should()
+          .dependOnClassesThat().resideInAPackage("java..")
+          .as("the library should depend only on core Java classes")
+          .because("users appreciate not experiencing a dependency hell")
+          .check(classes);
     }
 
     @Test
     void shouldHaveSinglePackage() {
-        SINGLE_PACKAGE_RULE.check(classes);
+        classes()
+          .should().resideInAPackage("com.pivovarit.collectors")
+          .check(classes);
     }
 }
