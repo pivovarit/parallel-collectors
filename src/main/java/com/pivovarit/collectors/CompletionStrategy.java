35cdffbe2806e2dba85ed8fd644132beacd27b24
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-09-20 10:27:12 +0200

    Seal CompletionStrategy interface

--- src/main/java/com/pivovarit/collectors/CompletionStrategy.java
+++ src/main/java/com/pivovarit/collectors/CompletionStrategy.java
@@ -6,13 +6,31 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-interface CompletionStrategy<T> extends Function<List<CompletableFuture<T>>, Stream<T>> {
+sealed interface CompletionStrategy<T>
+  extends Function<List<CompletableFuture<T>>, Stream<T>>
+  permits CompletionStrategy.Unordered, CompletionStrategy.Ordered {
 
     static <R> CompletionStrategy<R> unordered() {
-        return futures -> StreamSupport.stream(new CompletionOrderSpliterator<>(futures), false);
+        return new Unordered<>();
     }
 
     static <R> CompletionStrategy<R> ordered() {
-        return futures -> futures.stream().map(CompletableFuture::join);
+        return new Ordered<>();
+    }
+
+    final class Unordered<R> implements CompletionStrategy<R> {
+
+        @Override
+        public Stream<R> apply(List<CompletableFuture<R>> futures) {
+            return StreamSupport.stream(new CompletionOrderSpliterator<>(futures), false);
+        }
+    }
+
+    final class Ordered<R> implements CompletionStrategy<R> {
+
+        @Override
+        public Stream<R> apply(List<CompletableFuture<R>> futures) {
+            return futures.stream().map(CompletableFuture::join);
+        }
     }
 }
