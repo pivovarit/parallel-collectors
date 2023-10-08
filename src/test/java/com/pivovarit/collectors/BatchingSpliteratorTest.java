b945f67aeabd273ec48b8cd4e5947e019e1b2129
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-10-08 10:16:42 +0200

    Add BatchSpliterator presizing tests (#782)

--- src/test/java/com/pivovarit/collectors/BatchingSpliteratorTest.java
+++ src/test/java/com/pivovarit/collectors/BatchingSpliteratorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 import static com.pivovarit.collectors.BatchingSpliterator.partitioned;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,33 +15,33 @@ class BatchingSpliteratorTest {
 
     @Test
     void shouldSplitInNEvenBatches() {
-        List<Integer> list = Stream.generate(() -> 42).limit(10).collect(Collectors.toList());
+        var list = IntStream.range(0, 10).boxed().toList();
 
-        List<List<Integer>> result = partitioned(list, 3).collect(Collectors.toList());
+        var result = partitioned(list, 3).toList();
 
         assertThat(result)
-            .hasSize(3)
-            .extracting(List::size)
-            .contains(4, 3);
+          .hasSize(3)
+          .extracting(List::size)
+          .contains(4, 3);
     }
 
     @Test
     void shouldSplitInNBatches() {
-        List<Integer> list = Stream.generate(() -> 42).limit(10).collect(Collectors.toList());
+        var list = IntStream.range(0, 10).boxed().toList();
 
-        List<List<Integer>> result = partitioned(list, 2).collect(Collectors.toList());
+        var result = partitioned(list, 2).toList();
 
         assertThat(result)
-            .hasSize(2)
-            .extracting(List::size)
-            .containsOnly(5);
+          .hasSize(2)
+          .extracting(List::size)
+          .containsOnly(5);
     }
 
     @Test
     void shouldSplitInNSingletonLists() {
-        List<Integer> list = Stream.generate(() -> 42).limit(5).collect(Collectors.toList());
+        var list = IntStream.range(0, 5).boxed().toList();
 
-        List<List<Integer>> result = partitioned(list, 10).collect(Collectors.toList());
+        var result = partitioned(list, 10).toList();
 
         assertThat(result)
           .hasSize(5)
@@ -51,15 +51,29 @@ class BatchingSpliteratorTest {
 
     @Test
     void shouldReturnNestedListIfOneBatch() {
-        List<Integer> list = Stream.generate(() -> 42).limit(10).collect(Collectors.toList());
+        var list = IntStream.range(0, 10).boxed().toList();
 
-        List<List<Integer>> result = partitioned(list, 1).collect(Collectors.toList());
+        var result = partitioned(list, 1).toList();
 
-        assertThat(result.get(0)).containsExactlyElementsOf(list);
+        assertThat(result.getFirst()).containsExactlyElementsOf(list);
     }
 
     @Test
     void shouldReturnEmptyIfZeroParts() {
         assertThatThrownBy(() -> partitioned(Arrays.asList(1, 2, 3), 0).collect(Collectors.toList()));
+    }
+
+    @Test
+    void shouldReportCorrectSizeWhenOneBatch() {
+        var list = IntStream.range(0, 10).boxed().toList();
+
+        assertThat(partitioned(list, 1).count()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldReportCorrectSizeWhenMultipleBatches() {
+        var list = IntStream.range(0, 10).boxed().toList();
+
+        assertThat(partitioned(list, 2).count()).isEqualTo(2);
     }
 }
