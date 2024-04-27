3a858f9deb38235a3f92f0bf3fefd86c31893498
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-04-27 08:58:36 +0200

    Control parallelism of a separate virtual thread

--- src/main/java/com/pivovarit/collectors/ParallelStreamCollector.java
+++ src/main/java/com/pivovarit/collectors/ParallelStreamCollector.java
@@ -57,7 +57,10 @@ class ParallelStreamCollector<T, R> implements Collector<T, List<CompletableFutu
 
     @Override
     public BiConsumer<List<CompletableFuture<R>>, T> accumulator() {
-        return (acc, e) -> acc.add(dispatcher.enqueue(() -> function.apply(e)));
+        return (acc, e) -> {
+            dispatcher.start();
+            acc.add(dispatcher.enqueue(() -> function.apply(e)));
+        };
     }
 
     @Override
@@ -70,7 +73,10 @@ class ParallelStreamCollector<T, R> implements Collector<T, List<CompletableFutu
 
     @Override
     public Function<List<CompletableFuture<R>>, Stream<R>> finisher() {
-        return completionStrategy;
+        return acc -> {
+            dispatcher.stop();
+            return completionStrategy.apply(acc);
+        };
     }
 
     @Override
