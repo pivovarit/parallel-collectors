8f1695d6311e61075ed424fb7fcc2c6081b0ebf0
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-05-15 17:10:37 +0200

    Remove unused imports

--- src/test/java/com/pivovarit/collectors/ArchitectureTest.java
+++ src/test/java/com/pivovarit/collectors/ArchitectureTest.java
@@ -4,11 +4,9 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.junit.jupiter.api.Disabled;
 
 import static com.tngtech.archunit.core.domain.JavaModifier.FINAL;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-
 
 @AnalyzeClasses(packages = "com.pivovarit", importOptions = ImportOption.DoNotIncludeTests.class)
 class ArchitectureTest {
