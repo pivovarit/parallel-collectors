ec1f1ae1d2c2c388e2062c5d3bf7d182a54afa80
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-09-16 12:50:20 +0200

    Rewrite completion-order streaming tests (#947)

--- src/test/java/com/pivovarit/collectors/FunctionalTest.java
+++ src/test/java/com/pivovarit/collectors/FunctionalTest.java
@@ -2,40 +2,27 @@ package com.pivovarit.collectors;
 
 import com.pivovarit.collectors.ParallelCollectors.Batching;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 
-import static com.pivovarit.collectors.ParallelCollectors.parallel;
-import static com.pivovarit.collectors.ParallelCollectors.parallelToStream;
-import static com.pivovarit.collectors.TestUtils.returnWithDelay;
 import static com.pivovarit.collectors.TestUtils.withExecutor;
 import static java.lang.String.format;
-import static java.time.Duration.ofMillis;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 import static java.util.stream.Stream.of;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 /**
@@ -66,59 +53,6 @@ class FunctionalTest {
           batchStreamingTests((m, e, p) -> adaptAsync(Batching.parallelToStream(m, e, p)), format("ParallelCollectors.Batching.parallelToStream(p=%d)", PARALLELISM)),
           batchStreamingTests((m, e, p) -> adaptAsync(Batching.parallelToOrderedStream(m, e, p)), format("ParallelCollectors.Batching.parallelToOrderedStream(p=%d)", PARALLELISM))
         ).flatMap(i -> i);
-    }
-
-    @Test
-    void shouldCollectInCompletionOrder() {
-        // given
-        try (var executor = threadPoolExecutor(4)) {
-            List<Integer> result = of(350, 200, 0, 400)
-              .collect(parallelToStream(i -> returnWithDelay(i, ofMillis(i)), executor, 4))
-              .limit(2)
-              .toList();
-
-            assertThat(result).isSorted();
-        }
-    }
-
-    @Test
-    void shouldCollectEagerlyInCompletionOrder() {
-        // given
-        var executor = threadPoolExecutor(4);
-        AtomicBoolean result = new AtomicBoolean(false);
-        CompletableFuture.runAsync(() -> {
-            of(1, 10000, 1, 0)
-              .collect(parallelToStream(i -> returnWithDelay(i, ofMillis(i)), executor, 2))
-              .forEach(i -> {
-                  if (i == 1) {
-                      result.set(true);
-                  }
-              });
-        });
-
-        await()
-          .atMost(1, SECONDS)
-          .until(result::get);
-    }
-
-    @Test
-    void shouldExecuteEagerlyOnProvidedThreadPool() {
-        try (var executor = Executors.newFixedThreadPool(2)) {
-            var countingExecutor = new CountingExecutor(executor);
-            var executions = new AtomicInteger();
-            var list = List.of("A", "B");
-
-            list.stream()
-              .collect(parallel(s -> {
-                  executions.incrementAndGet();
-                  return s;
-              }, countingExecutor, 1))
-              .join()
-              .forEach(__ -> {});
-
-            assertThat(countingExecutor.getInvocations()).isEqualTo(1);
-            assertThat(executions.get()).isEqualTo(2);
-        }
     }
 
     private static <R extends Collection<Integer>> Stream<DynamicTest> batchTests(CollectorSupplier<Function<Integer, Integer>, Executor, Integer, Collector<Integer, ?, CompletableFuture<R>>> collector, String name) {
@@ -158,35 +92,8 @@ class FunctionalTest {
           .supplyAsync(stream::toList, Executors.newSingleThreadExecutor()));
     }
 
-    private static ThreadPoolExecutor threadPoolExecutor(int unitsOfWork) {
-        return new ThreadPoolExecutor(unitsOfWork, unitsOfWork,
-          0L, MILLISECONDS,
-          new LinkedBlockingQueue<>());
-    }
-
     @FunctionalInterface
     interface CollectorSupplier<T1, T2, T3, R> {
         R apply(T1 t1, T2 t2, T3 t3);
-    }
-
-    private static class CountingExecutor implements Executor {
-
-        private final AtomicInteger counter = new AtomicInteger();
-
-        private final Executor delegate;
-
-        public CountingExecutor(Executor delegate) {
-            this.delegate = delegate;
-        }
-
-        @Override
-        public void execute(Runnable command) {
-            counter.incrementAndGet();
-            delegate.execute(command);
-        }
-
-        public Integer getInvocations() {
-            return counter.get();
-        }
     }
 }
