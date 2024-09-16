96332be1be1341b56533526c58a00244087434a5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-09-16 12:48:46 +0200

    Rewrite completion-order streaming tests

--- src/test/java/com/pivovarit/collectors/test/StreamingTest.java
+++ src/test/java/com/pivovarit/collectors/test/StreamingTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,7 +16,10 @@ import java.util.stream.Stream;
 
 import static com.pivovarit.collectors.TestUtils.returnWithDelay;
 import static com.pivovarit.collectors.test.StreamingTest.CollectorDefinition.collector;
+import static java.time.Duration.ofMillis;
 import static java.time.Duration.ofSeconds;
+import static java.util.stream.Stream.of;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 class StreamingTest {
@@ -25,6 +29,22 @@ class StreamingTest {
           collector("parallelToStream()", (f) -> ParallelCollectors.parallelToStream(f)),
           collector("parallelToStream(e)", (f) -> ParallelCollectors.parallelToStream(f, e())),
           collector("parallelToStream(e, p)", (f) -> ParallelCollectors.parallelToStream(f, e(), p())),
+          collector("parallelToOrderedStream()", (f) -> ParallelCollectors.parallelToOrderedStream(f)),
+          collector("parallelToOrderedStream(e)", (f) -> ParallelCollectors.parallelToOrderedStream(f, e())),
+          collector("parallelToOrderedStream(e, p)", (f) -> ParallelCollectors.parallelToOrderedStream(f, e(), p()))
+        );
+    }
+
+    private static Stream<CollectorDefinition<Integer, Integer>> allCompletionOrderStreaming() {
+        return Stream.of(
+          collector("parallelToStream()", (f) -> ParallelCollectors.parallelToStream(f)),
+          collector("parallelToStream(e)", (f) -> ParallelCollectors.parallelToStream(f, e())),
+          collector("parallelToStream(e, p)", (f) -> ParallelCollectors.parallelToStream(f, e(), p()))
+        );
+    }
+
+    private static Stream<CollectorDefinition<Integer, Integer>> allOrderedStreaming() {
+        return Stream.of(
           collector("parallelToOrderedStream()", (f) -> ParallelCollectors.parallelToOrderedStream(f)),
           collector("parallelToOrderedStream(e)", (f) -> ParallelCollectors.parallelToOrderedStream(f, e())),
           collector("parallelToOrderedStream(e, p)", (f) -> ParallelCollectors.parallelToOrderedStream(f, e(), p()))
@@ -46,6 +66,31 @@ class StreamingTest {
                 .pollInterval(Duration.ofMillis(10))
                 .atMost(Duration.ofMillis(100))
                 .until(() -> counter.get() > 0);
+          }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> shouldCollectInCompletionOrder() {
+        return allCompletionOrderStreaming()
+          .map(c -> DynamicTest.dynamicTest(c.name(), () -> {
+              var result = of(300, 200, 0, 400)
+                .collect(c.factory().collector(i -> returnWithDelay(i, ofMillis(i))))
+                .toList();
+
+              assertThat(result).isSorted();
+          }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> shouldCollectInOriginalOrder() {
+        return allOrderedStreaming()
+          .map(c -> DynamicTest.dynamicTest(c.name(), () -> {
+              var source = List.of(300, 200, 0, 400);
+              var result = source.stream()
+                .collect(c.factory().collector(i -> returnWithDelay(i, ofMillis(i))))
+                .toList();
+
+              assertThat(result).containsExactlyElementsOf(source);
           }));
     }
 
