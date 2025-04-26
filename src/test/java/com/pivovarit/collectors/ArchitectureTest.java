a28383714e1c695c7bd6c9e557087c47c667a55c
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-04-26 07:52:22 +0200

    Ensure that default CompletableFuture executors are not used

--- src/test/java/com/pivovarit/collectors/ArchitectureTest.java
+++ src/test/java/com/pivovarit/collectors/ArchitectureTest.java
@@ -4,6 +4,14 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static com.tngtech.archunit.core.domain.JavaModifier.FINAL;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
@@ -53,4 +61,22 @@ class ArchitectureTest {
     static final ArchRule shouldHaveTwoPublicClasses = classes()
       .that().haveSimpleName("ParallelCollectors").or().haveSimpleName("Batching")
       .should().bePublic().andShould().haveModifier(FINAL);
+
+    @ArchTest
+    static final ArchRule noDefaultExecutorInCompletableFuture = ArchRuleDefinition.noClasses()
+      .should().callMethod(CompletableFuture.class, "supplyAsync", Supplier.class)
+      .orShould().callMethod(CompletableFuture.class, "runAsync", Runnable.class)
+      .orShould().callMethod(CompletableFuture.class, "thenRunAsync", Runnable.class)
+      .orShould().callMethod(CompletableFuture.class, "thenAcceptAsync", Consumer.class)
+      .orShould().callMethod(CompletableFuture.class, "thenApplyAsync", Function.class)
+      .orShould().callMethod(CompletableFuture.class, "thenCombineAsync", CompletionStage.class, BiFunction.class)
+      .orShould().callMethod(CompletableFuture.class, "runAfterBothAsync", CompletionStage.class, Runnable.class)
+      .orShould().callMethod(CompletableFuture.class, "thenAcceptBothAsync", CompletionStage.class, Consumer.class)
+      .orShould().callMethod(CompletableFuture.class, "thenComposeAsync", CompletionStage.class)
+      .orShould().callMethod(CompletableFuture.class, "completeAsync", Supplier.class)
+      .orShould().callMethod(CompletableFuture.class, "exceptionallyComposeAsync", Function.class)
+      .orShould().callMethod(CompletableFuture.class, "handleAsync", BiFunction.class)
+      .orShould().callMethod(CompletableFuture.class, "applyToEitherAsync", CompletionStage.class, Function.class)
+      .orShould().callMethod(CompletableFuture.class, "whenCompleteAsync", BiConsumer.class)
+      .because("those default to ForkJoinPool.commonPool() which is a terrible default for most cases - consider Executors#newVirtualThreadPerTaskExecutor instead. If you really need ForkJoinPool, provide it explicitly (ForkJoinPool.commonPool())");
 }
