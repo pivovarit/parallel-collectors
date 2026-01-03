978a01daf2dde85297ac3dbebee1073921e47eab
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-01-03 13:28:53 +0100

    Expand tests

--- src/test/java/com/pivovarit/collectors/OptionTest.java
+++ src/test/java/com/pivovarit/collectors/OptionTest.java
@@ -1,17 +1,13 @@
 package com.pivovarit.collectors;
 
-import java.util.Optional;
-import java.util.OptionalInt;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -46,11 +42,11 @@ class OptionTest {
     @TestFactory
     Stream<DynamicTest> shouldThrowWhenSameOptionsAreUsedMultipleTimes() {
         return Stream.<Options.CollectingOption>of(Options.batched(), Options.executor(r -> {}), Options.parallelism(1))
-            .map(o -> DynamicTest.dynamicTest("should handle duplicated: " + nameOf(o), () -> {
-                assertThatThrownBy(() -> ConfigProcessor.process(o, o))
-                  .isInstanceOf(IllegalArgumentException.class)
-                  .hasMessageContaining("each option can be used at most once, and you configured '%s' multiple times".formatted(nameOf(o)));
-            }));
+          .map(o -> DynamicTest.dynamicTest("should handle duplicated: " + nameOf(o), () -> {
+              assertThatThrownBy(() -> ConfigProcessor.process(o, o))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("each option can be used at most once, and you configured '%s' multiple times".formatted(nameOf(o)));
+          }));
     }
 
     private String nameOf(Options.CollectingOption option) {
@@ -60,42 +56,5 @@ class OptionTest {
             case Options.ThreadPool __ -> "executor";
             case Options.Ordered __ -> "ordered";
         };
-    }
-
-    @Nested
-    class ConfigTests {
-
-        record TestData(Boolean batching, Integer parallelism, Executor executor) {
-        }
-
-        @TestFactory
-        Stream<DynamicTest> shouldNotAllowCreatingConfigurationWithNullOptionals() {
-            return testData()
-              .map(data ->
-                DynamicTest.dynamicTest("Batching[%s], Parallelism[%s], Executor[%s]".formatted(printable(data.batching), printable(data.parallelism), printable(data.executor)),
-                () -> Assertions.assertThatThrownBy(() -> new ConfigProcessor.Config(null, data.batching, data.parallelism, data.executor)).isInstanceOf(NullPointerException.class)
-              ));
-        }
-
-        private static Stream<TestData> testData() {
-            var batchings = Stream.of(null, null, true).toList();
-            var parallelisms = Stream.of(null, null, 42).toList();
-            var executors = Stream.of(null, null, (Executor)r -> {}).toList();
-
-            return batchings.stream()
-              .flatMap(b -> parallelisms.stream()
-                .flatMap(p -> executors.stream()
-                  .map(e -> new TestData(b, p, e))))
-              .filter(data -> data.batching == null || data.parallelism == null || data.executor == null);
-        }
-
-        private String printable(Object o) {
-            return switch (o) {
-                case null -> "null";
-                case Optional<?> opt -> opt.map(obj -> "some(%s)".formatted(obj instanceof Executor ? "executor" : obj.toString())).orElse("empty");
-                case OptionalInt opt -> opt.isPresent() ? "some(%d)".formatted(opt.getAsInt()) : "empty";
-                default -> o.toString();
-            };
-        }
     }
 }
